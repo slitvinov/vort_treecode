2325b13f70a1d1e1db0e133e4ef00ad6d8c1c578
Sergey Litvinov <slitvinov@gmail.com>
2022-05-26 14:31:32 -0400

    tool/read.py: dump vtk file

--- tool/read.py
+++ tool/read.py
@@ -3,8 +3,6 @@ import struct
 import numpy as np
 
 REAL = 'd'
-
-
 def read_real(f, n):
     buf = f.read(struct.calcsize(REAL * n))
     return struct.unpack(REAL * n, buf)
@@ -15,6 +13,7 @@ def read_int(f, n):
     return struct.unpack('i' * n, buf)
 
 
+nodes = []
 with open(sys.argv[1], 'rb') as f:
     del2, t, dt0, vel0, dt, vel, *rest = read_real(f, 8)
     version, sheet_cnt, *rest = read_int(f, 8)
@@ -24,8 +23,27 @@ with open(sys.argv[1], 'rb') as f:
         for fil_ind in range(fil_cnt):
             alpha, dgamma, *rest = read_real(f, 8)
             node_cnt, *rest = read_int(f, 8)
-            sys.stderr.write("node_cnt: %d\n" % node_cnt)
             buf = f.read(struct.calcsize(4 * REAL * node_cnt))
             node = np.ndarray((node_cnt, 4), REAL, buf)
-            # for t, x, y, z in node:
-            #    print(x, y, z)
+            nodes.append(node)
+
+nnodes = sum(len(node) for node in nodes)
+nbytes = sum(3 * node.nbytes // 4 for node in nodes)
+
+with open(sys.argv[2], 'wb+') as f:
+    f.write(b"""\
+# vtk DataFile Version 2.0
+generate with tool/read.py
+BINARY
+DATASET POLYDATA
+POINTS %d double
+""" % nnodes)
+    offset = f.tell()
+    f.seek(nbytes - 1, 1)
+    f.write(b'\0')
+
+pos = np.memmap(sys.argv[2], '>' + REAL, 'r+', offset, (nnodes, 3))
+shift = 0
+for node in nodes:
+    np.copyto(pos[shift:shift + len(node), :], node[:, 1:])
+    shift += len(node)
